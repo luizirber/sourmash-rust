b9d4b14eaa649e27ae1bd78a1fb1490903648294
Luiz Irber <luiz.irber@gmail.com>
2018-06-08 18:33:01 +0000

    don't add extra_count

--- src/from.rs
+++ src/from.rs
@@ -8,7 +8,7 @@ impl From<MinHashKmers> for KmerMinHash {
 
       let mut new_mh = KmerMinHash::new(
         values.len() as u32,
-        values.get(0).unwrap().kmer.len() as u32, 
+        values.get(0).unwrap().kmer.len() as u32,
         false,
         42,
         0,
@@ -16,7 +16,7 @@ impl From<MinHashKmers> for KmerMinHash {
       );
 
       let hash_with_abunds = values.iter()
-                                   .map(|x| (x.hash as u64, (x.count + x.extra_count) as u64))
+                                   .map(|x| (x.hash as u64, x.count as u64))
                                    .collect();
 
       new_mh.add_many_with_abund(hash_with_abunds);
